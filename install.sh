e6ba152e850550de4e2e3698b8a9c001fae4382f
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-03-26 16:31:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,16 @@ fi
 echo "Installing packages"
 apt-get -y install php5 php5-fpm php5-mysql
 
+echo "Removing default apache2 configuration"
+sudo rm /etc/apache2/sites-available/000-default.conf
+echo "Adding new configuration"
+sudo mv dist/scripts/000-default.conf /etc/apache2/sites-available/000-default.conf
+sudo chmod 644 /etc/apache2/sites-available/000-default.conf
+
+echo "Changing webserver rights"
+sudo chown -R pi:www-data /home/pi/piscreen-server/webserver
+sudo chmod u+rxw,g+rx-w,o-rwx /home/pi/piscreen-server/webserver
+
 echo "Creating startup scripts"
 mv dist/scripts/piscreen-server-api.service /lib/systemd/system/piscreen-server-api.service
 sudo chmod 644 /lib/systemd/system/piscreen-server-api.service
