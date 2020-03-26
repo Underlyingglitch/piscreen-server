4f3a9234f479685dc2c2b3930e887f3384a4d53f
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-03-26 20:14:25 +0100

    Changed webconfiguration

--- install.sh
+++ install.sh
@@ -16,10 +16,6 @@ echo "Adding new configuration"
 sudo mv dist/scripts/000-default.conf /etc/apache2/sites-available/000-default.conf
 sudo chmod 644 /etc/apache2/sites-available/000-default.conf
 
-echo "Changing webserver rights"
-sudo chown -R pi:www-data /home/pi/piscreen-server/webserver
-sudo chmod u+rxw,g+rx-w,o-rwx /home/pi/piscreen-server/webserver
-
 echo "Creating startup scripts"
 mv dist/scripts/piscreen-server-api.service /lib/systemd/system/piscreen-server-api.service
 sudo chmod 644 /lib/systemd/system/piscreen-server-api.service
