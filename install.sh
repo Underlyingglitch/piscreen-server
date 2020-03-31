f4be82d7eb90ba00dcaded84d48b9b540fd9843e
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-03-31 11:09:11 +0200

    Added controlpanel config

--- install.sh
+++ install.sh
@@ -10,24 +10,23 @@ fi
 echo "Installing packages"
 apt-get -y install php
 
-echo "Removing default apache2 configuration"
-sudo rm /etc/apache2/sites-available/000-default.conf
-echo "Adding new configuration"
-sudo mv dist/scripts/000-default.conf /etc/apache2/sites-available/000-default.conf
-sudo chmod 644 /etc/apache2/sites-available/000-default.conf
-
-echo "Changing webserver rights"
-sudo chown -R pi:www-data /home/pi/piscreen-server/webserver/controlpanel
+echo "Removing apache2"
+sudo apt-get remove apache2
+sudo apt-get purge apache2
 
 echo "Creating startup scripts"
 mv dist/scripts/piscreen-server-api.service /lib/systemd/system/piscreen-server-api.service
+mv dist/scripts/piscreen-server-controlpanel.service /lib/systemd/system/piscreen-server-controlpanel.service
 sudo chmod 644 /lib/systemd/system/piscreen-server-api.service
+sudo chmod 644 /lib/systemd/system/piscreen-server-controlpanel.service
 chmod +x /home/pi/piscreen-server/webserver/startapiserver.py
+chmod +x /home/pi/piscreen-server/webserver/startcontrolpanel.py
 
 echo "Reloading startup sequence"
 systemctl daemon-reload
 
 echo "Starting services"
 systemctl enable piscreen-server-api.service
+systemctl enable piscreen-server-controlpanel.service
 systemctl start piscreen-server-api.service
-sudo service apache2 restart
+systemctl start piscreen-server-controlpanel.service
---
+++ webserver/startcontrolpanel.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('php -S 0.0.0.0:80 -t /home/pi/piscreen-server/webserver/controlpanel/')
