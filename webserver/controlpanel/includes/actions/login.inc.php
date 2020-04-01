d5f45aec49d7fa0b06b644b4d86c6efba6156b91
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-04-01 19:16:06 +0200

    Initial commit for login script

--- webserver/controlpanel/includes/actions/login.inc.php
+++ webserver/controlpanel/includes/actions/login.inc.php
@@ -1,5 +1,25 @@
 <?php
 
+$username = htmlspecialchars(stripslashes($_POST['username']));
+$password = htmlspecialchars(stripslashes($_POST['password']));
 
+$data_path = "../../../data/";
+
+$json = file_get_contents($data_path.'users/controlpanel_users.json');
+$userarray = json_decode($json, true);
+
+for ($i=0; $i<count($userarray); $i++) {
+  if ($userarray[$i]['username'] === $username) {
+    if ($userarray[$i]['password'] === $_POST['password']) {
+      //Details correct!
+    } else {
+      //Incorrect password
+      header("Location: ../../login.php?error=incorrectpassword");
+    }
+  } else {
+    //Incorrect username
+    header("Location: ../../login.php?error=usernotfound");
+  }
+}
 
 ?>
