b9c75a7cc47b7f64ec11442944c7506eb313df65
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-05-21 16:07:21 +0200

    Update media.php

--- webserver/controlpanel/media.php
+++ webserver/controlpanel/media.php
@@ -81,48 +81,7 @@ if (!$auth->isAnyRole("media")) {
               <h6 class="m-0 font-weight-bold text-primary">Alle gebruikers</h6>
             </div>
             <div class="card-body">
-              <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                  <thead>
-                    <tr>
-                      <th>Naam</th>
-                      <th>Laatste inlog</th>
-                      <th>Laatste IP</th>
-                      <th>Acties</th>
-                    </tr>
-                  </thead>
-                  <tfoot>
-                    <tr>
-                      <th>Naam</th>
-                      <th>Laatste inlog</th>
-                      <th>Laatste IP</th>
-                      <th>Acties</th>
-                    </tr>
-                  </tfoot>
-                  <tbody id="users">
-                    <?php
-
-                    $data_path = "../data/";
-
-                    $json = file_get_contents($data_path.'users/controlpanel_users.json');
-                    $userarray = json_decode($json, true);
-
-                    for ($i=0; $i<count($userarray); $i++) {
-                      ?>
-                      <tr>
-                        <td><?php echo $userarray[$i]['username']; ?></td>
-                        <td><?php echo $userarray[$i]['last_login']; ?></td>
-                        <td><?php echo $userarray[$i]['last_ip_address']; ?></td>
-                        <td><button class="btn btn-primary change-password-btn" username="<?php echo $userarray[$i]['username']; ?>">Reset wachtwoord</button> <button class="btn btn-info role-edit-btn" username="<?php echo $userarray[$i]['username']; ?>">Rollen</button><?php if ($userarray[$i]['blocked'] == 0) { ?> <button class="btn btn-warning block-user-btn" username="<?php echo $userarray[$i]['username']; ?>">Blokkeer</button><?php } else { ?> <button class="btn btn-success activate-user-btn" username="<?php echo $userarray[$i]['username']; ?>">Activeer</button><?php } ?> <button class="btn btn-danger delete-btn" username="<?php echo $userarray[$i]['username']; ?>">Verwijder</button></td>
-                      </tr>
-
-                      <?php
-                    }
-
-                    ?>
-                  </tbody>
-                </table>
-              </div>
+            
             </div>
           </div>
 
