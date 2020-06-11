e28792edf2450c7fe0ee6255c2c2705080b3e667
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-06-11 19:13:36 +0200

    Deleted old files

--- webserver/startapiserver.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.system('php -S 0.0.0.0:31804 -t /home/pi/piscreen-server/webserver/api/')
--- webserver/startcontrolpanel.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.system('php -S 0.0.0.0:80 -t /home/pi/piscreen-server/webserver/controlpanel/')
