4283f9c8181e0e00ef247d79dc58d7ea23a16c5b
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-06-14 18:58:47 +0200

    Updated logout function

--- webserver/controlpanel/players.php
+++ webserver/controlpanel/players.php
@@ -160,15 +160,15 @@ if (!$auth->isAnyRole("players")) {
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Uitloggen</h5>
           <button class="close" type="button" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span>
           </button>
         </div>
-        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+        <div class="modal-body">Klik op <strong>Uitloggen</strong> om definitief uit te loggen!</div>
         <div class="modal-footer">
-          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="login.html">Logout</a>
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Annuleren</button>
+          <a class="btn btn-warning" href="logout.php">Uitloggen</a>
         </div>
       </div>
     </div>
