d63fd57f04256ace0e2c41d4adf10d592f9d58ce
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-06-20 20:10:31 +0200

    Updated playlists module

--- webserver/controlpanel/playlists.php
+++ webserver/controlpanel/playlists.php
@@ -70,15 +70,15 @@ if (!$auth->isAnyRole("playlists")) {
 
           <!-- Page Heading -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-2 text-gray-800">Players</h1>
+            <h1 class="h3 mb-2 text-gray-800">Afspeellijsten</h1>
             <button id="create-player-toggle" class="d-none d-sm-inline-block btn btn-sm btn-success shadow-sm"><i class="fas fa-plus fa-sm text-white-50"></i> Nieuwe player</button>
           </div>
-          <p class="mb-4">Bekijk en voeg nieuwe players toe aan uw PiScreen installatie.</p>
+          <p class="mb-4">Bekijk, wijzig en voeg nieuwe afspeellijsten toe aan uw PiScreen installatie.</p>
 
           <!-- DataTales Example -->
           <div class="card shadow mb-4">
             <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">Alle gebruikers</h6>
+              <h6 class="m-0 font-weight-bold text-primary">Alle afspeellijsten</h6>
             </div>
             <div class="card-body">
               <div class="table-responsive">
@@ -86,16 +86,16 @@ if (!$auth->isAnyRole("playlists")) {
                   <thead>
                     <tr>
                       <th>Naam</th>
-                      <th>Laatste inlog</th>
-                      <th>Laatste IP</th>
+                      <th>Media (aantal)</th>
+                      <th>Players</th>
                       <th>Acties</th>
                     </tr>
                   </thead>
                   <tfoot>
                     <tr>
                       <th>Naam</th>
-                      <th>Laatste inlog</th>
-                      <th>Laatste IP</th>
+                      <th>Media (aantal)</th>
+                      <th>Players</th>
                       <th>Acties</th>
                     </tr>
                   </tfoot>
@@ -158,15 +158,15 @@ if (!$auth->isAnyRole("playlists")) {
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Uitloggen</h5>
           <button class="close" type="button" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span>
           </button>
         </div>
-        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+        <div class="modal-body">Klik op <strong>Uitloggen</strong> om definitief uit te loggen!</div>
         <div class="modal-footer">
-          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="login.html">Logout</a>
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Annuleren</button>
+          <a class="btn btn-warning" href="logout.php">Uitloggen</a>
         </div>
       </div>
     </div>
@@ -189,7 +189,7 @@ if (!$auth->isAnyRole("playlists")) {
   <!-- Page level custom scripts -->
   <script src="js/demo/datatables-demo.js"></script>
 
-  <script src="js/users.js"></script>
+  <script src="js/playlists.js"></script>
 
 </body>
 
