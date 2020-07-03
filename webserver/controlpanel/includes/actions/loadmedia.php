809961b2e26c334bb04f52f600ac45c6a8977e36
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-07-03 10:42:26 +0200

    Removed comments

--- webserver/controlpanel/includes/actions/loadmedia.php
+++ webserver/controlpanel/includes/actions/loadmedia.php
@@ -1,13 +1,5 @@
 <?php
 
-// $content = file_get_contents("../../../data/media/uploads/".$_GET['requested']);
-// $content = base64_encode($content);
-//
-// $data = "data:image/png;base64,{$content}";
-//
-// header('Content-Type: image/png');
-// echo base64_decode($data);
-
 $remoteImage = "/var/www/data/media/uploads/".$_GET['requested'];
 $imginfo = getimagesize($remoteImage);
 header("Content-type: {$imginfo['mime']}");
