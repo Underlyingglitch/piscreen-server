deb2ac06663690a985e3b0da884f9a7c382a6f44
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-07-03 19:11:42 +0200

    Added type property to uploaded media

--- webserver/controlpanel/includes/actions/saveplayerinfo.php
+++ webserver/controlpanel/includes/actions/saveplayerinfo.php
@@ -5,7 +5,7 @@ if (isset($_POST['code']) && isset($_POST['ip']) && isset($_POST['name'])) {
   $json = file_get_contents("/var/www/data/players/players.json");
   $playerarray = json_decode($json, true);
 
-  $newdata = array("name" => htmlspecialchars(stripslashes($_POST['name'])), "ip" => htmlspecialchars(stripslashes($_POST['ip'])), "code" => htmlspecialchars(stripslashes($_POST['code'])));
+  $newdata = array("name" => htmlspecialchars(stripslashes($_POST['name'])), "ip" => htmlspecialchars(stripslashes($_POST['ip'])), "code" => htmlspecialchars(stripslashes($_POST['code'])), "active_playlist" => "");
 
   array_push($playerarray, $newdata);
 
