807d8510e3c77e7db3577724210234a33efa3235
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-07-05 13:03:20 +0200

    Added security

--- webserver/controlpanel/includes/actions/deletemedia.php
+++ webserver/controlpanel/includes/actions/deletemedia.php
@@ -1,10 +1,25 @@
 <?php
 
+session_start();
+
+include "../classes/auth.php";
+
+$auth = new Auth();
+
 $id = $_POST['id'];
 
-//TODO: Add security
-
 $media = json_decode(file_get_contents("/var/www/data/media/media.json"), true);
+
+$users = json_decode(file_get_contents("/var/www/data/controlpanel_users.sjon"), true);
+
+$username = $users[$_SESSION['id']]['username'];
+
+if (
+  (!$auth->isRole("delete_own_media") && !$auth->isRole("delete_all_media")) ||
+  (!$auth->isRole("delete_all_media") && $auth->isRole("delete_own_media") && $media[(int)$id]['username'] != $username)
+) {
+  header("Location: ../../norole.php");
+}
 
 shell_exec("rm /var/www/data/media/uploads/".$media[(int)$id]['filename']);
 
