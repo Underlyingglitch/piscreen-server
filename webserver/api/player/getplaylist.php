6ef394a98ad9ff57bc85a5abe8345dc730896cdf
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-07-13 13:48:40 +0200

    Update client playlist scripts added

--- webserver/api/player/getplaylist.php
+++ webserver/api/player/getplaylist.php
@@ -1,15 +1,15 @@
 <?php
 
 $code = $_GET['code'];
-$hash = $_GET['hash'];
 
 $players = json_decode(file_get_contents("/var/www/data/players.json"), true);
 $playlists = json_decode(file_get_contents("/var/www/data/playlists.json"), true);
 
-if (md5(json_encode($playlists[$players[$code]['active_playlist']])) == $hash) {
-  echo "false";
+
+if ($players[$code]['active_playlist'] == "") {
+  echo "empty";
 } else {
-  echo "true";
+  echo json_encode($playlists[$players[$code]['active_playlist']]);
 }
 
 ?>
